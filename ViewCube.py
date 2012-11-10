6bfc138d8462c1edeb9fef69c38c8c7823737eb3
Oleksandr Korobov <korobov.alex@gmail.com>
2012-11-10 12:29:14 +0200

    Changing description.

--- ViewCube.py
+++ ViewCube.py
@@ -1,4 +1,10 @@
 __author__ = 'oleksandr'
 
+# The idea of this class is to visualize cubic rubica states and transformations
+# using VPython or pygame or something else. Cube class supports injecting observer
+# object which is in fact just an event processing subsystem. To make visualizer 
+# work properly developer should use contract (Cube will notify instance of this 
+# class automaticaly)
+
 class Visualizer:
     pass
